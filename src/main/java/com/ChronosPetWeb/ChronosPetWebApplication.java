c3083ed519c24c409c13bbe0dab5f96503dbc86d
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2026-04-04 16:13:43 -0300

    Inicializando repositorio com repositorio basico spring

---
+++ src/main/java/com/ChronosPetWeb/ChronosPetWebApplication.java
@@ -0,0 +1,13 @@
+package com.ChronosPetWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosPetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosPetWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChronosPetWeb/ChronosPetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChronosPetWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosPetWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
