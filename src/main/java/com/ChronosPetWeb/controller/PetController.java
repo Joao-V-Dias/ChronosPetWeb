c3be92273c8dc91e97b578d97baf40b3c8865d74
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2026-04-04 18:13:47 -0300

    Annotate PetController, add home endpoint

--- src/main/java/com/ChronosPetWeb/controller/PetController.java
+++ src/main/java/com/ChronosPetWeb/controller/PetController.java
@@ -1,12 +1,13 @@
 package com.ChronosPetWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ChronosPetWeb.service.PetService;
 
-import org.springframework.ui.Model;
-
+@Controller
 public class PetController {
     
     @Autowired
@@ -16,5 +17,10 @@ public class PetController {
     public String index(Model model){
         model.addAttribute("petList", petService.getAllPets());
         return "pet/index";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Olá! O Spring Boot está rodando!";
     }
 }
